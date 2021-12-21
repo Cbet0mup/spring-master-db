b90c34836245b7140b1fd509949438cd00c25ac0
Павел Лютов <cbet0mup.pro@gmail.com>
2021-12-21 23:58:56 +0700

    0059 правка контроллера

--- src/main/java/ru/database/springmasterdb/controllers/ManagementController.java
+++ src/main/java/ru/database/springmasterdb/controllers/ManagementController.java
@@ -1,11 +1,17 @@
 package ru.database.springmasterdb.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.database.springmasterdb.dto.WorkOrderDtoPresent;
 import ru.database.springmasterdb.services.EngineerServiceImpl;
 import ru.database.springmasterdb.services.ReceiverServiceImpl;
+
+import java.util.List;
 
 @RestController
 //@CrossOrigin(origins = "http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf")
@@ -21,4 +27,10 @@ public class ManagementController {
         this.engineerService = engineerService;
         this.receiverService = receiverService;
     }
+    @GetMapping("/findworkorder/all")  // найти все выданные за период
+    public ResponseEntity<List<WorkOrderDtoPresent>> getAllWorkOrders() {
+        List<WorkOrderDtoPresent> workOrderDTOpr = null;// workOrderServiceImpl.findAllWorkOrders();
+        return new ResponseEntity<>(workOrderDTOpr, HttpStatus.OK);
+    }
+
 }
